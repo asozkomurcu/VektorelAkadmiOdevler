0db1f564828a52384de6227d71502c52c9d28b21
asozkomurcu <114032649+asozkomurcu@users.noreply.github.com>
2023-06-20 08:18:15 +0300

    Add files via upload

---
+++ OOPKalitimOdevi/OOPKalitimOdevi/StajyerPersonel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPKalitimOdevi
+{
+    public class StajyerPersonel : PersonBase, IMaasHesapla
+    {
+        
+        public double Saat { get; set; }
+        public double Saatlik { get; set; }
+
+        public StajyerPersonel()
+        {
+            
+        }
+
+        public StajyerPersonel(string id, string firstName, string lastName,double saat,double saatlik) : base(id, firstName, lastName)
+        {
+            Saat = saat;
+            Saatlik = saatlik;
+        }
+
+        
+
+        public double MaasHesapla() => Math.Round((Saat * Saatlik),2);
+
+        public override string AdSoyadGetir()
+        {
+            return $"{base.AdSoyadGetir()}\nBir Ayda Çalıştığı Saat : {Saat}\nSaatlik Ücreti : {Saatlik} TL\nAylık Maaşı : {MaasHesapla()} TL";
+        }
+    }
+}
